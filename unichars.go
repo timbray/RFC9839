d3d189efcd2d026c7e43b29e753065f081102c8b
Tim Bray <tbray@textuality.com>
2025-05-29 10:56:46 -0700

    adress offline feedback
    Signed-off-by: Tim Bray <tbray@textuality.com>

--- unichars.go
+++ unichars.go
@@ -45,12 +45,12 @@ type runePair struct {
 // contain runes being queried are moved to the front. "most likely" is
 // strictly based on Tim's intuition, there's no quantitative data behind it.
 
-var unicodeScalars = []*runePair{
+var unicodeScalars = []runePair{
 	{0, 0xD7FF},        // most of the BMP
 	{0xE000, 0x10FFFF}, // mostly astral planes
 }
 
-var xmlChars = []*runePair{
+var xmlChars = []runePair{
 	{0x20, 0xD7FF},      // most of the BMP
 	{0xA, 0xA},          // newline
 	{0xE000, 0xFFFD},    // BMP after surrogates
@@ -59,7 +59,7 @@ var xmlChars = []*runePair{
 	{0x10000, 0x10FFFF}, // astral planes
 }
 
-var unicodeAssignables = []*runePair{
+var unicodeAssignables = []runePair{
 	{0x20, 0x7E},       // ASCII
 	{0xA, 0xA},         // newline
 	{0xA0, 0xD7FF},     // most of the BMP
@@ -85,10 +85,28 @@ var unicodeAssignables = []*runePair{
 	{0x100000, 0x10FFFD},
 }
 
-func pairContains(pair *runePair, r rune) bool {
+// …which raises issues of how you might optimize the current "simplest thing
+// that could possibly work" implementation. I refuse to touch the code until
+// I hear about a scenario where this constitutes a detectable performance problem.
+// You could think about having three bit arrays but see
+// https://medium.com/@val_deleplace/7-ways-to-implement-a-bit-set-in-go-91650229b386
+// Plus, no matter how tight you pack, you're looking at ~400K of data structures.
+// The worst case would probably be checking a large volume of Chinese text for
+// UnicodeAssignable, because those code-points are at the third position in the
+// subset just above, so there will be 4 wasted comparison operations for each
+// code point.
+// Having said that, the current implementation has excellent data locality and will
+// thus be very CPU cache friendly.
+// The craziest idea (and my favorite) is to build up a sample of which runePairs are
+// being used the most and then from time to time shuffle the most-used to the front of the
+// subset slice. You'd need an AtomicPointer update to be thread-safe.
+// But like I said, I refuse to do anything without a sample corpus that is causing
+// a measurable delay.
+
+func pairContains(pair runePair, r rune) bool {
 	return r >= pair.lo && r <= pair.hi
 }
-func subsetContains(subset []*runePair, r rune) bool {
+func subsetContains(subset []runePair, r rune) bool {
 	for _, pair := range subset {
 		if pairContains(pair, r) {
 			return true
@@ -97,14 +115,16 @@ func subsetContains(subset []*runePair, r rune) bool {
 	return false
 }
 
-func isStringInSubset(s string, subset []*runePair) bool {
+func isStringInSubset(s string, subset []runePair) bool {
 	return isUTF8InSubset([]byte(s), subset)
 }
-func isUTF8InSubset(u []byte, subset []*runePair) bool {
+func isUTF8InSubset(u []byte, subset []runePair) bool {
 	index := 0
 	for index < len(u) {
 		r, width := utf8.DecodeRune(u[index:])
 		if r == 0xFFFD && width == 1 {
+			// this is how the utf8 pkg signals invalid UTF8 bytes, notably
+			// including surrogate values
 			return false
 		}
 		if !subsetContains(subset, r) {
